4a0b973b7aa4a11c015fb1e599f7b16445a3e1a9
Raphael Ventura <raphaelventura55@gmail.com>
2022-11-15 15:21:28 +0100

    task manager 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,25 +3,40 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def list
-    @tasks.each do |i|
-      return i
-    end
+  def show
+    @tasks = Task.find(params[:id])
   end
 
   def new
     @tasks = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
-    @tasks = task.new(params[:task])
+    @tasks = Task.new(task_params)
     @tasks.save
     redirect_to task_path(@task)
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    redirect_to restaurant_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to restaurants_path, status: :see_other
+  end
+
   private
 
-def task_params
-  params.require(:task).permit(:title, :details, :complete)
-end
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
